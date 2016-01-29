19217642bc7d18aba07e7abfcb63fee0cee270bc
Uri <uri@speedy.net>
2016-01-29 13:38:00 +0200

    Added space.

--- speedypedia/main/index.php
+++ speedypedia/main/index.php
@@ -92,7 +92,7 @@ $hebrew_alpha = "אלפא";
     <meta name="theme-color" content="#b2cafa">
     <script type="text/javascript">
         if (top != self) {
-            top.location= self.location;
+            top.location = self.location;
         }
     </script>
 </head>
